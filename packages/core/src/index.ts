e4f4a25dd309b8375ec344cbfc2f8333b4eb19f4
irohafox <matsukawa@grabss.co.jp>
2025-07-04 11:44:05 +0900

    Refac

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,9 @@
+// @grabstream/core
+// Core library for grabstream packages
+
+// Logger utility
+export * from './logger'
+// Message types and guards
 export * from './messages'
+// Validation constants and functions
 export * from './validation'
