84e75166f95fe19177da1f1c2d27ea4a4d2b03b4
irohafox <matsukawa@grabss.co.jp>
2025-07-14 12:53:08 +0900

    WIP: init room component

--- playground/client/src/routes/[roomId]/+page.svelte
+++ playground/client/src/routes/[roomId]/+page.svelte
@@ -1,4 +1,21 @@
 <script lang="ts">
+  import { page } from '$app/state'
+
+  let { data } = $props()
+  const { grabstreamClient } = data
+
+  $effect(() => {
+   (async () => {
+      await grabstreamClient.joinRoom(page.params.roomId)
+    })()
+
+    return () => {
+      if (grabstreamClient.isJoined) {
+        grabstreamClient.leaveRoom()
+      }
+    }
+  })
 </script>
 
 <h1>TODO: /[roomId]</h1>
+<a href="/">to /</a>
