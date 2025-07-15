1179dfff85c72c2195b125acafdc6aaa9241364c
irohafox <matsukawa@grabss.co.jp>
2025-07-15 15:25:48 +0900

    WIP: CommonLoading

---
+++ playground/client/src/lib/components/CommonLoading.svelte
@@ -0,0 +1,28 @@
+<div class="loader"></div>
+
+<style lang="scss">
+  .loader {
+    position: relative;
+    width: 30px;
+    height: 30px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: -3px;
+      left: -3px;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      border: 0.25em solid var(--color-border);
+      border-top-color: var(--color-primary);
+      animation: spinner 1.5s linear infinite;
+    }
+  }
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
--- playground/client/src/routes/[roomId]/+page.svelte
+++ playground/client/src/routes/[roomId]/+page.svelte
@@ -1,39 +1,35 @@
 <script lang="ts">
 import { page } from '$app/state'
 import { GrabstreamClient } from '@grabstream/client'
+import CommonLoading from '$lib/components/CommonLoading.svelte';
 
 const grabstreamClient = new GrabstreamClient({
   url: 'http://localhost:8080'
 })
 
+const joinRoom = async () => {
+  await new Promise(resolve => setTimeout(resolve, 3000))
+  if (!grabstreamClient.isConnected) {
+    await grabstreamClient.connect()
+  }
+  await grabstreamClient.joinRoom(page.params.roomId)
+  // TODO: joinedRoom or Error handling
+}
+
 $effect(() => {
-  ;(async () => {
-    if (!grabstreamClient.isConnected) {
-      try {
-        await grabstreamClient.connect()
-      } catch (error) {
-        console.error(error)
-        alert('Failed to connect to grabstream server.')
-        return
-      }
-    }
-
-    try {
-      await grabstreamClient.joinRoom(page.params.roomId)
-    } catch (error) {
-      console.error(error)
-      alert('Failed to join the room.')
-    }
-  })()
-
   return () => {
-    if (grabstreamClient.isJoined) {
-      grabstreamClient.leaveRoom()
-    }
-    grabstreamClient.disconnect()
+    if (grabstreamClient.isJoined) grabstreamClient.leaveRoom()
+    if (grabstreamClient.isConnected) grabstreamClient.disconnect()
   }
 })
 </script>
 
-<h1>TODO: /[roomId]</h1>
-<a href="/">to /</a>
+<section class="h-100 mx-md d-flex flex-column items-center justify-center">
+  {#await joinRoom()}
+    <CommonLoading />
+  {:then}
+    <h1>Joined Room: {page.params.roomId}</h1>
+  {:catch error}
+    <h1 class="fs-lg fw-bold text-muted">Error: {error.message}</h1>
+  {/await}
+</section>
